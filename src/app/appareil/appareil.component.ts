2d51f62631f3ac91b84d86d2b968e7adf41d4b22
Haddock13 <webmaster.cslg13@free.fr>
2020-12-21 17:23:35 +0100

    Liaison à double sens

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appareil',
@@ -7,13 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppareilComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    this.appareilName = "Indéfini";
+    this.appareilStatus = "Inconnu";
+  }
 
   ngOnInit(): void {
   }
 
-  appareilName: string = "Machine à laver";
-  appareilStatus: string = 'éteint';
+  @Input() appareilName: string;
+  @Input() appareilStatus: string;
 
   getStatus() {
     return this.appareilStatus;
