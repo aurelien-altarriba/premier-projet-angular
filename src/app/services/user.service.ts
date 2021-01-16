3c404dc06187fa1ea952e0927fede42024467aa4
Aurélien Altarriba <aurelien.altarriba@gmail.com>
2021-01-16 19:03:45 +0100

    Ajout appareil - Forms méthode réactive

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,24 @@
+import { Subject } from "rxjs";
+import { User } from "../models/User.model";
+
+export class UserService {
+  private users: User[];
+  public userSubject: Subject<User[]>;
+
+  constructor() {
+    this.users = [
+      new User('Will', 'Alexander', 'will@will.com', 'jus d\'orange', ['coder', 'boire du café'])
+    ];
+
+    this.userSubject = new Subject<User[]>();
+  }
+
+  emitUsers() {
+    this.userSubject.next(this.users.slice());
+  }
+
+  addUser(user: User) {
+    this.users.push(user);
+    this.emitUsers();
+  }
+}
